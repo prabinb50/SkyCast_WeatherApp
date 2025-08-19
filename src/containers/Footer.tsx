43c7acbdff3fa7ae79acd6f56b64528fa6dfc2a1
Prabin Joshi <joshiprabin17@gmail.com>
2025-08-19 18:59:53 +0545

    fix: adjust gap size in footer section for better alignment

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<section className="flex-center gap-[0.8rem] p-[40px] mt-[60px] flex-col sm:flex-row">
+		<section className="flex-center gap-[0.2rem] p-[40px] mt-[60px] flex-col sm:flex-row">
 			<p className="text-[0.8rem]">Designed and developed by</p>
 			<a
 				className="text-[1rem]"
